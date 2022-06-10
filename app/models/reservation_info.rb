e4d81a322499dae1facf76cd1079b27be9409aa4
新添太雅 <niizoetaiga@TaigaNiizoe.local>
2022-06-10 17:20:13 +0900

    DB追加

--- app/models/reservation_info.rb
+++ app/models/reservation_info.rb
@@ -4,6 +4,8 @@ class ReservationInfo < ApplicationRecord
   include Discard::Model
   belongs_to :reservation
   belongs_to :car_size
+  has_many :reservation_car_wash_options
+  has_many :car_wash_options, through: :reservation_car_wash_options
 
   accepts_nested_attributes_for :reservation_car_wash_options, allow_destroy: true
 
